8fc35857284e18b5849a87ce92cd39ed201f97dd
Sergejs Tiselskis <slokas85@gmail.con>
2022-09-27 18:51:14 +0300

    HomeWorkView is added.

---
+++ AwardsCollectionApp/HomeWorkView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeWorkView.swift
+//  AwardsCollectionApp
+//
+//  Created by John Doe on 27/09/2022.
+//
+
+import SwiftUI
+
+struct HomeWorkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeWorkView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeWorkView()
+    }
+}
